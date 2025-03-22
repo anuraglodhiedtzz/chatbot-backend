013ae91ddfb5bfd44bdc8581ba811c60d4a42624
Anurag Lodhi <you@example.com>
2025-03-22 19:16:11 +0530

    🚀 Fix: Added sqlite3 dependency

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
-import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import sqlite3 from "sqlite3";
 
 // ✅ Open SQLite database connection
 const db = await open({
     filename: "./orders.db",
-    driver: sqlite3.Database,
+    driver: sqlite3.Database, // ✅ Correct Driver
 });
 
-// ✅ Export as default
 export default db;
