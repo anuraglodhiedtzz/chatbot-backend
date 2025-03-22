d088a986b8344df625deda0b97cb36cfe4c64b2b
Anurag Lodhi <you@example.com>
2025-03-22 19:21:49 +0530

    📦 Fix: Replaced sqlite with sqlite3

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 import { open } from "sqlite";
 import sqlite3 from "sqlite3";
 
-// ✅ Open SQLite database connection
 const db = await open({
     filename: "./orders.db",
-    driver: sqlite3.Database, // ✅ Correct Driver
+    driver: sqlite3.Database,
 });
 
 export default db;
